9a46e21fc0343d0c7e3a639613ebb08169c44bfc
Luthais Bradbury <68658961+internet-cabbage@users.noreply.github.com>
2026-03-11 22:47:57 +0000

    Add files via upload

--- Heat_Equation.py
+++ Heat_Equation.py
@@ -6,13 +6,13 @@ from matplotlib.animation import FuncAnimation
 # Dimensions of discrete grid
 dimensions = (300,300)
 
-dt = 0.001
+dt = 0.00001
 
 
 
 # Size of plate in metres
 size=(1,1)
-thermalDiffusivity = 0.001
+thermalDiffusivity = 0.01
 
 
 hGridSpacing = 1/dimensions[0]
@@ -25,8 +25,10 @@ plate = np.zeros(dimensions)
 plate.fill(273)
 dTempPlate = plate
 
+tempRange = (273, 340)
 
-temps = np.random.uniform(273,300,(dimensions[0]-2, dimensions[1]-2))
+
+temps = np.random.uniform(tempRange[0],tempRange[1],(dimensions[0]-2, dimensions[1]-2))
 plate[1:-1, 1:-1] = temps
 
 def tempChange():
@@ -44,24 +46,45 @@ def tempChange():
     dTemp = thermalDiffusivity * (tempDDotVert + tempDDotHor) * dt
     plate[1:-1, 1:-1] = plate[1:-1, 1:-1] + dTemp
 
-print(plate)
+# Click handler
+fig = plt.figure(figsize=(10,6))
+ax = fig.add_subplot(1,1,1)
+
+brushRadius = 8
+
+def onclick(event):
+    if event.button == 1:
+        x = int(np.round(event.xdata, 0))
+        y = int(np.round(event.ydata, 0))
+
+        yStart = max(0, y-brushRadius)
+        yEnd = min(dimensions[1], y + brushRadius)
+
+        xStart= max(0, x-brushRadius)
+        xEnd = min(dimensions[0], x + brushRadius)
+
+        if 0 <= x < dimensions[0] and 0 <= y < dimensions[1]:
+            plate[yStart: yEnd, xStart: xEnd] = tempRange[1]
+
+#print(plate)
 
 np.set_printoptions(precision=3)
 
-fig = plt.figure(figsize=(10,6))
-ax = fig.add_subplot(1,1,1)
+
+fig.canvas.mpl_connect('button_press_event',onclick)
+
 
 img = ax.imshow(plate, cmap='hot', animated=True)
 plt.colorbar(img)
 
-
+stepsPerFrame = 30
 def update(frame):
-    tempChange()
+    for i in range(stepsPerFrame):
+        tempChange()
     img.set_data(plate)
     return [img]
 
-animation = FuncAnimation(fig, update, interval=100, blit=True)
+animation = FuncAnimation(fig, update, interval=10, blit=True)
 plt.show()
-
 
 

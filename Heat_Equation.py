b23a227537c26e8d801f28473bd91967fb913b6f
Luthais Bradbury <68658961+internet-cabbage@users.noreply.github.com>
2026-04-09 13:59:07 +0100

    Add files via upload
    Much more customisable!!!

--- Heat_Equation.py
+++ Heat_Equation.py
@@ -3,17 +3,18 @@ import time
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Dimensions of discrete grid
-dimensions = (300,300)
-
-dt = 0.00001
 
 
+
+# -----------------------------------------------------------
+# Parameters
+# -----------------------------------------------------------
 
 # Size of plate in metres
-size=(1,1)
-thermalDiffusivity = 0.01
+size=(10,10)
 
+# Number of grid points
+dimensions = (300,300)
 
 hGridSpacing = 1/dimensions[0]
 dh = hGridSpacing * size[0]
@@ -21,15 +22,104 @@ dh = hGridSpacing * size[0]
 vGridSpacing = 1/dimensions[1]
 dv = hGridSpacing * size[1]
 
+
+
+# Time interval between evaluations
+dt = 0.01
+
+# Frames between image update
+frameSkip = 1
+
+accuracyV = dt / (dv**2)
+accuracyH = dt / (dh**2)
+print(f'If either of these are large, panic: {accuracyV, accuracyH} (btw I consider larger than ~10 to be too large)')
+
+scenarioNames = ('(1) Random Temps', 
+                 '(2) Random Temps but with additional customisability',
+                 '(3) Grid with cooler bar in the centre',
+                 '(4) Gaussian heat packet')
+nLine = '\n'.join(scenarioNames)
+text = f"\n ============================== \n\n Scenarios are:\n \n {nLine} \n \n ============================== \n"
+print(text)
+
+selection = int(input('Enter the number of the scenario: '))
+
 plate = np.zeros(dimensions)
-plate.fill(273)
-dTempPlate = plate
-
-tempRange = (273, 340)
 
 
-temps = np.random.uniform(tempRange[0],tempRange[1],(dimensions[0]-2, dimensions[1]-2))
-plate[1:-1, 1:-1] = temps
+
+# ======================= Scenario 1: Random temps
+
+if selection == 1:
+    plate.fill(273)
+    tempRange = (273,340)
+
+    # Randomly generate temperatures
+    temps = np.random.uniform(tempRange[0], tempRange[1], (dimensions[0]-2, dimensions[1] -2))
+    
+    # [1:-1, 1:-1] means that all values except the edge values, have the new temps assigned to them
+    plate[1:-1, 1:-1] = temps
+    print(plate)
+    thermalDiffusivity = 0.003
+    
+
+# ======================= Scenario 2: Random temps (but more customisable)
+
+if selection == 2:
+    edgeTemp = float(input('Edge temp: '))
+    plate.fill(edgeTemp)
+    minTemp = float(input('Random minimum temp: '))
+    maxTemp = float(input('Random maximum temp: '))
+    tempRange = (minTemp,maxTemp)
+
+    # Randomly generate temperatures
+    temps = np.random.uniform(tempRange[0], tempRange[1], (dimensions[0]-2, dimensions[1] -2))
+    
+    # [1:-1, 1:-1] means that all values except the edge values, have the new temps assigned to them
+    plate[1:-1, 1:-1] = temps
+    thermalDiffusivity = float(input('Thermal diffusivity (Ideally a low value ~0.1): '))
+
+# ======================= Scenario 3: Cooler central bar
+
+if selection == 3:
+    plate.fill(273)
+    tempRange = (273,340)
+
+    # Randomly generate temperatures
+    temps = np.random.uniform(tempRange[0], tempRange[1], (dimensions[0]-2, dimensions[1] -2))
+    
+    # [1:-1, 1:-1] means that all values except the edge values, have the new temps assigned to them
+    plate[1:-1, 1:-1] = temps
+
+    # Cooler central horizontal bar
+
+    barTemp = 260
+
+    relativeVSize = 0.2
+    halfVPoints = int(dimensions[1] * relativeVSize * 0.5)
+    midPoint = dimensions[1] // 2
+    
+    barTop = midPoint + halfVPoints
+    barBottom = midPoint - halfVPoints
+
+    # Set bar temp
+    plate[barBottom:barTop, 1:-1] = barTemp
+    
+
+
+    print(plate)
+    thermalDiffusivity = 0.003
+
+
+
+
+
+
+
+
+
+
+
 
 def tempChange():
 
@@ -78,9 +168,13 @@ img = ax.imshow(plate, cmap='hot', animated=True)
 plt.colorbar(img)
 
 stepsPerFrame = 30
+time = 0.0
 def update(frame):
+    global time
     for i in range(stepsPerFrame):
         tempChange()
+        time = time + dt
+        print(time)
     img.set_data(plate)
     return [img]
 
